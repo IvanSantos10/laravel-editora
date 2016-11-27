51e9185c03d57390ea46fd7d514138f570f34d9e
Ivan Santos <ivannildosanto@hotmail.com>
2016-11-27 18:16:11 -0300

    Criando editara com laravel 5.3

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use editora\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 1)->create([
+            'name' => 'Admin Editora',
+            'email' => 'admin@editora.com'
+        ]);
+    }
+}
