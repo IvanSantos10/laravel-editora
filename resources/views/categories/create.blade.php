ee338d7347b65f46e014877fccd9750eb277321a
Ivan Santos <ivannildosanto@hotmail.com>
2016-11-28 17:23:02 -0300

    Macro para form-group

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -7,15 +7,16 @@
 
             {!! Form::open(['route' => 'categories.store', 'class' => 'form']) !!}
 
-            <div class="form-group{{$errors->first('name') ? ' has-error' : ''}}">
-                {!! Form::label('name', 'Nome', ['class' => 'control-label']) !!}
-                {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                {!! Form::error('name', $errors) !!}
-            </div>
+                {!! Html::openFormGroup('name', $errors) !!}
+                    {!! Form::label('name', 'Nome', ['class' => 'control-label']) !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                    {!! Form::error('name', $errors) !!}
+                {!! Html::closeFormGroup() !!}
 
-            <div class="form-group">
-                {!! Form::submit('Criar categoria', ['class' => 'btn btn-primary']) !!}
-            </div>
+                {!! Html::openFormGroup() !!}
+                    {!! Form::submit('Criar categoria', ['class' => 'btn btn-primary']) !!}
+                {!! Html::closeFormGroup() !!}
+
             {!! Form::close() !!}
         </div>
     </div>
